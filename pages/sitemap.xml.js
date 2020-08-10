500a3d00dd1b0325d7117147d460e96612a97b4c
Ryan <hello@ryanjordal.me>
2020-08-10 17:50:32 -0400

    update sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,23 +1,26 @@
-import React from "react";
 import client from "../client";
 
-const EXTERNAL_DATA_URL = "https://jsonplaceholder.typicode.com/posts";
-
 const createSitemap = (posts) => {
-  console.log("posts", posts[0].slug.current);
   return `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     <url>
+        <loc>${`https://ryanjordal.me/`}</loc>
+        <changefreq>weekly</changefreq>
+    </url>
+    <url>
         <loc>${`https://ryanjordal.me/posts`}</loc>
+        <changefreq>weekly</changefreq>
     </url>
     <url>
         <loc>${`https://ryanjordal.me/projects`}</loc>
+        <changefreq>monthly</changefreq>
     </url>
         ${posts
           .map((post) => {
             return `
                     <url>
                         <loc>${`https://ryanjordal.me/post/${post.slug.current}/`}</loc>
+                        <lastmod>${post["_updatedAt"].split("T")[0]}</lastmod>
                     </url>
                 `;
           })
@@ -31,7 +34,7 @@ const Sitemap = () => {
 
 export default Sitemap;
 
-export async function getStaticProps({ res }) {
+export async function getServerSideProps({ res }) {
   const posts = await client.fetch(
     `
     *[_type == "post"]
@@ -41,5 +44,5 @@ export async function getStaticProps({ res }) {
   res.setHeader("Content-Type", "text/xml");
   res.write(createSitemap(posts));
   res.end();
-  return {};
+  return { props: {} };
 }
